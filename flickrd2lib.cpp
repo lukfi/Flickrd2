c5496584251881efe1426864183d0c829dbeff5d
Lukasz Finster <lukasz.finster@gmail.com>
2023-08-23 09:17:05 +0200

    flickrd2lib implementation

--- flickrd2lib.cpp
+++ flickrd2lib.cpp
@@ -3,20 +3,31 @@
 #include "utils/datetime.h"
 #include "regexp/regexp.h"
 
+namespace Fd2 {
+#include "utils/SimpleJSON/json.hpp"
+};
+
 #define ENABLE_SDEBUG
 #define DEBUG_PREFIX "Flickrd2: "
 #include "utils/screenlogger.h"
 
-void ParseFor(const std::string& str, const std::string& exp)
+#define STATE(x) StateGuard s(this, x); if (!s.Valid()) return;
+
+std::string ParseFor(const std::string& str, const std::string& exp)
 {
-    //std::string exp = "\"csrf\":\"(.*)\"";
+    std::string ret;
     LF::re::RegExp reg(exp);
     int r = reg.FindAll(str);
-    SDEB("FOUND: %d", r);
-    for (int i = 0; i < r; ++i)
+    //SDEB("FOUND: %d", r);
+    //for (int i = 0; i < r; ++i)
+    //{
+    //    SINFO("%d -> %s", i, reg.Cap(i).c_str());
+    //}
+    if (r > 1)
     {
-        SINFO("%d -> %s", i, reg.Cap(i).c_str());
+        ret = reg.Cap(1);
     }
+    return ret;
 }
 
 class GetData
@@ -27,6 +38,8 @@ public:
 
 void LF::Flickrd2::LoadCookies(Browser_t browser)
 {
+    STATE(InternalState_t::LoadCookies)
+
     BrowserHacker hacker(BrowserHacker::Browser_t::Vivaldi);
     if (hacker.BrowserValid())
     {
@@ -51,11 +64,21 @@ void OnRequestComplete(void* userData, LF::www::UrlGet::RequestResult_t res)
     {
         f.Put((uint8_t*)data->mGet->GetResponse().mCurrentResponse.data(), data->mGet->GetResponse().mCurrentResponse.size());
     }
-    ParseFor(data->mGet->GetResponse().mCurrentResponse, "\"csrf\":\"(.*)\"");
+    std::string siteKey = ParseFor(data->mGet->GetResponse().mCurrentResponse, "root\\.YUI_config\\.flickr\\.api\\.site_key = \"(.*)\";");
+    SINFO("KEY: %s", siteKey.c_str());
+    std::string ret = ParseFor(data->mGet->GetResponse().mCurrentResponse, "root\\.auth = (.*);");
+    if (!ret.empty())
+    {
+        //SINFO(ret.c_str());
+        Fd2::json::JSON j = Fd2::json::JSON::Load(ret);
+        SINFO("SIGNED: %d", j.at("signedIn").ToBool());
+    }
 }
 
-bool LF::Flickrd2::LoadApiKey()
+void LF::Flickrd2::LoadApiKey()
 {
+    STATE(InternalState_t::LoadApiKey)
+
     mGet = std::make_shared<LF::www::UrlGet>();
     mGet->SetCookies(mCookies);
     
@@ -64,5 +87,22 @@ bool LF::Flickrd2::LoadApiKey()
     
     CONNECT(mGet->REQUEST_COMPLETE, OnRequestComplete);
     mGet->Get("https://www.flickr.com/", true, data);
-    return false;
+}
+
+void LF::Flickrd2::SetState(InternalState_t state)
+{
+    auto toStr = [](InternalState_t state) -> std::string 
+    {
+        switch (state)
+        {
+        case InternalState_t::IDLE:
+            return "IDLE";
+        case InternalState_t::LoadCookies:
+            return "LoadCookies";
+        case InternalState_t::LoadApiKey:
+            return "LoadApiKey";
+        }
+    };
+    SINFO("State %s -> %s", toStr(mState).c_str(), toStr(state).c_str());
+    mState = state;
 }
--- flickrd2lib.h
+++ flickrd2lib.h
@@ -15,15 +15,55 @@ enum class Browser_t
 class Flickrd2
 {
 public:
-    Flickrd2()
+    Flickrd2() : mState(InternalState_t::IDLE)
     {}
 
     void LoadCookies(Browser_t browser);
-    bool LoadApiKey();
+    void LoadApiKey();
 
 private:
+    enum class InternalState_t
+    {
+        IDLE,
+        LoadCookies,
+        LoadApiKey
+    };
+
+    class StateGuard
+    {
+    public:
+        StateGuard(Flickrd2* app, InternalState_t state) :
+            mApp(app)
+        {
+            std::lock_guard<std::mutex> lock(mApp->mStateLock);
+            if (mApp->mState == InternalState_t::IDLE)
+            {
+                mApp->SetState(state);
+                mValid = true;
+            }
+        }
+        ~StateGuard()
+        {
+            if (mValid)
+            {
+                std::lock_guard<std::mutex> lock(mApp->mStateLock);
+                mApp->SetState(InternalState_t::IDLE);
+            }
+        }
+        bool Valid() const { return mValid; }
+    private:
+        Flickrd2* mApp;
+        bool mValid {false};
+    };
+
     std::shared_ptr<NetworkCookies> mCookies;
     std::shared_ptr<LF::www::UrlGet> mGet;
+
+    void SetState(InternalState_t state);
+    std::mutex mStateLock;
+    std::atomic<InternalState_t> mState;
+
+    friend class StateGuard;
 };
 
 }
